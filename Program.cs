be9531619ff5efc54e2c141a716a848c4cefa008
sam charles <c.sam.charles@gmail.com>
2019-01-28 12:19:34 -0800

    data base connection works

--- Program.cs
+++ Program.cs
@@ -60,7 +60,7 @@ namespace Titanoboa
         static void Main(string[] args)
         {
             //DB config -- TODO
-            string connectionString = "";
+            string connectionString = "Server=localhost;Database=scaley_abilities;Uid=scaley;Pwd=abilities;";
 
             //Establish DB Connection
             connection = new MySqlConnection(connectionString);
@@ -71,15 +71,15 @@ namespace Titanoboa
                 connection.Open();
                 
                 //Run through queue -- TODO
-                while(!Queue.isempty())
-                {
-                    string packet = Queue.pop();
-                    RunCommands(packet);
-                }
+                // while(!Queue.isempty())
+                // {
+                //     string packet = Queue.pop();
+                //     RunCommands(packet);
+                // }
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                Console.WriteLine(ex.ToString());
+                Console.Error.WriteLine(e.Message);
             }
 
             connection.Close();
