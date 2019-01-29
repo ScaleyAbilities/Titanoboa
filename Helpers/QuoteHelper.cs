510290a77caf4ba517f7d9281a68404ecf0688cb
Michael Baart <michael.baart96@gmail.com>
2019-01-28 17:53:55 -0800

    Add establish connection with quote server

---
+++ Helpers/QuoteHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+
+namespace Titanoboa
+{
+    public static class QuoteHelper
+    {
+        private static string quotePort = "1234";
+        private static string quoteServer = "quote"
+        
+    }
+}
