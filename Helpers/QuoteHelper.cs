dd74b4f6c5087bea771cfeed5121ec42ccf3b8cc
Michael Baart <michael.baart96@gmail.com>
2019-01-28 18:11:19 -0800

    Finish connecting to quote server, idk if it works

--- Helpers/QuoteHelper.cs
+++ Helpers/QuoteHelper.cs
@@ -1,15 +1,34 @@
 using System;
-using System.Data;
-using MySql.Data;
-using MySql.Data.MySqlClient;
-using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Titanoboa
 {
     public static class QuoteHelper
     {
-        private static string quotePort = "1234";
-        private static string quoteServer = "quote"
-        
+        // Quote server port number
+        private const int quotePort = "4448";
+        // Quote server url
+        private static string quoteServer = "quoteserve.seng.uvic.ca";
+
+        private bool connectionEstablished(){
+            // Got the follow lines of code from microsoft .Net documentation. Might not be best?
+            // https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-client-socket-example
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(quoteServer);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, quotePort);
+
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sender.Connect(remoteEP);
+        }
+
+        public static int Main(String[] args) {
+            if(connectionEstablished()){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
     }
 }
