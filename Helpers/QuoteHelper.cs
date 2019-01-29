3c2829e91cb9d5f40af1ba1ef389627fa6c70488
Michael Baart <michael.baart96@gmail.com>
2019-01-28 18:19:31 -0800

    Add try catch blocks

--- Helpers/QuoteHelper.cs
+++ Helpers/QuoteHelper.cs
@@ -11,24 +11,34 @@ namespace Titanoboa
         // Quote server url
         private static string quoteServer = "quoteserve.seng.uvic.ca";
 
+        private static Socket skt;
+
         private bool connectionEstablished(){
             // Got the follow lines of code from microsoft .Net documentation. Might not be best?
             // https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-client-socket-example
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(quoteServer);
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, quotePort);
+            try
+            {
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(quoteServer);
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, quotePort);
 
-            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            sender.Connect(remoteEP);
+                skt = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                skt.Connect(remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to establish connection with Quote Server: {0}", e.ToString());
+                return false;
+            }
+            return true;
         }
-
-        public static int Main(String[] args) {
-            if(connectionEstablished()){
-                return 0;
-            }
-            else{
-                return 1;
-            }
+    }
+    public static int Main(String[] args) {
+        if(connectionEstablished()){
+            return 0;
+        }
+        else{
+            return 1;
         }
     }
 }
