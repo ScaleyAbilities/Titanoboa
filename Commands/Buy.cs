1f71056063fc0506d289b7b12e8845918bc6ff34
sam charles <c.sam.charles@gmail.com>
2019-01-28 18:24:16 -0800

    sum pending transaction balance changes and account for them in get user balance

--- Commands/Buy.cs
+++ Commands/Buy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Data;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
 
 namespace Titanoboa
 {
@@ -9,36 +10,41 @@ namespace Titanoboa
     {
         /*
             Buy command flow:
-            1- Buy current user balance
+            1- Get current user balance
             2- Get current stock price
             3- Calculate stock amount (only whole stocks)
             3- Insert buy into transactions table, *set pending flag to true*
          */
-        public static void Buy(string userid, string stockSymbol, decimal amount) 
+        public static void Buy(string userid, JObject commandParams) 
         {
-            try
+            //Unpack JObject
+            var amount = (decimal)commandParams["amount"];
+            var stockSymbol = commandParams["stockSymbol"].ToString();
+
+            // Get users current balance
+            var balance = TransactionHelper.GetUserBalance(userid);
+            
+            //Check if user exists
+            if(balance == null)
             {
-                // Get users current balance
-                var userbalance = TransactionHelper.GetUserBalance(userid);
-                if(userbalance < amount)
-                {
-                    throw new System.InvalidOperationException("Insufficient funds.");
-                }
-
-                // Get current stock price -- TO DO in helper
-                var stockprice = (decimal)TransactionHelper.GetStockPrice(stockSymbol);
-                if(amount < stockprice)
-                {
-                    throw new System.InvalidOperationException("Not enough money for stock purchase.");
-                }
-
-                var stockAmount = (int)Math.Floor(amount/stockprice);
-
-                TransactionHelper.AddTransaction(userid, userbalance, stockSymbol, "BUY", stockAmount*stockprice, stockAmount, true);
+                throw new System.InvalidOperationException("User does not exist.");
+            } 
+            else if((decimal)balance < amount)
+            {
+                throw new System.InvalidOperationException("Insufficient funds.");
             }
-            catch (Exception e) {
-                Console.Error.WriteLine(e.Message);
+
+            // Get current stock price -- TO DO in helper
+            var stockPrice = (decimal)TransactionHelper.GetStockPrice(stockSymbol);
+            if(amount < stockPrice)
+            {
+                throw new System.InvalidOperationException("Not enough money for stock purchase.");
             }
+
+            var stockAmount = (int)Math.Floor(amount/stockPrice);
+            var balanceChange = stockAmount*stockPrice*-1;
+
+            TransactionHelper.AddTransaction(userid, (decimal)balance, stockSymbol, "BUY", balanceChange, stockAmount, true);
         } 
     }
 }
--- Helpers/TransactionHelper.cs
+++ Helpers/TransactionHelper.cs
@@ -8,7 +8,8 @@ namespace Titanoboa
 {
     public static class TransactionHelper
     {
-        public static bool AddTransaction(string userid, decimal balance, string stockSymbol, string commandText, decimal balanceChange, int stockAmount, bool pending){
+        public static bool AddTransaction(string userid, decimal balance, string stockSymbol, string commandText, decimal balanceChange, int stockAmount, bool pending)
+        {
             MySqlCommand command = SqlHelper.CreateSqlCommand();
             command.Prepare();
             command.CommandText = @"INSERT INTO transactions (userid, balance, stocksymbol, command, balancechange, stockamount, pending) 
@@ -28,7 +29,8 @@ namespace Titanoboa
             throw new NotImplementedException();
         }
 
-        public static bool AddUser(string userid, decimal balance) {
+        public static bool AddUser(string userid, decimal balance) 
+        {
             MySqlCommand command = SqlHelper.CreateSqlCommand();
             command.CommandText = "INSERT INTO users (balance, userid) Values ('@balance', '@userid')";
             command.Prepare();
@@ -37,18 +39,28 @@ namespace Titanoboa
             return SqlHelper.ExcecuteNonQuerySqlCommand(command);
         }
 
-        public static object GetUserBalance(string userid) {
+        public static object GetUserBalance(string userid) 
+        {
             MySqlCommand command = SqlHelper.CreateSqlCommand();
-            command.CommandText = "SELECT balance FROM users WHERE userid==@userid";
+
+            //Get balance minus pending transactions balance changes
+            command.CommandText = @"SELECT balance + SUM(IFNULL(transactions.balancechange, 0))  
+                                FROM users LEFT JOIN transactions ON users.id = transactions.usernum 
+                                AND transactions.transactiontime >= DATE_SUB(@curTime, INTERVAL 60 SECOND)  
+                                AND transactions.command = ""BUY"" 
+                                AND transactions.pendingflag = 1
+                                WHERE users.userid = @userid";
             command.Prepare();
+            command.Parameters.AddWithValue("@curTime", DateTime.Now);
             command.Parameters.AddWithValue("@userid", userid);
             var balance = SqlHelper.ExcecuteScalarSqlCommand(command);
             return balance;
         }
 
-        public static bool UpdateUserBalance(string userid, decimal balance) {
+        public static bool UpdateUserBalance(string userid, decimal balance) 
+        {
             MySqlCommand command = SqlHelper.CreateSqlCommand();
-            command.CommandText = "UPDATE users SET balance = @balance WHERE userid==@userid";
+            command.CommandText = "UPDATE users SET balance = @balance WHERE userid=@userid";
             command.Prepare();
             command.Parameters.AddWithValue("@balance", balance);
             command.Parameters.AddWithValue("@userid", userid);
