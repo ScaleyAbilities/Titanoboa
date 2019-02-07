d65a9ee23abdd91ac52758166e99cebaf36e0a58
Michael Baart <michael.baart96@gmail.com>
2019-02-06 17:10:26 -0800

    Fix more poo poo code

--- Helpers/QuoteHelper.cs
+++ Helpers/QuoteHelper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Titanoboa
@@ -29,6 +30,31 @@ namespace Titanoboa
             {
                 Console.WriteLine("Unable to establish connection with Quote Server: {0}", e.ToString());
             }
+        }
+
+        public static decimal GetQoute(User user, string stockSymbol){
+            var bytes = new byte[1024];
+            var msg = Encoding.ASCII.GetBytes(user + "," + stockSymbol);
+            
+            var bytesSent = skt.Send(msg);
+            var bytesRecv = skt.Receive(bytes);
+            
+            var msgRecv = Encoding.UTF8.GetString(bytes);
+            var recv = msgRecv.Split(',');
+
+            var amount = decimal.Parse(recv[0]);
+            var timestamp = UnixTimeStampToDateTime(double.Parse(recv[3]) / 1000);
+            var cryptokey = recv[4];
+            Program.Logger.LogQuoteServer(user, amount, stockSymbol, timestamp, cryptokey);
+            return amount;
+        }
+
+        private static DateTime UnixTimeStampToDateTime( double unixTimeStamp )
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+            return dtDateTime;
         }
     }
 }
