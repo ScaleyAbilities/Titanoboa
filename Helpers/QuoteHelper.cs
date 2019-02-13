a58e5a2643166693e4d1da75d50567ae67447abe
Morgan Zolob <mzolob@gmail.com>
2019-02-12 21:24:44 -0800

    Fix ordering maybe?

--- Helpers/QuoteHelper.cs
+++ Helpers/QuoteHelper.cs
@@ -49,9 +49,11 @@ namespace Titanoboa
                 var msgRecv = Encoding.UTF8.GetString(bytes).Replace("\0", string.Empty).Trim();
                 var recv = msgRecv.Split(',');
 
+                Console.WriteLine($"Quote Server Message: {msgRecv}");
+
                 amount = decimal.Parse(recv[0]);
-                var quoteStockSymbol = recv[1];
-                var quoteUserId = recv[2];
+                var quoteStockSymbol = recv[2];
+                var quoteUserId = recv[1];
                 var timestamp = recv[3];
                 var cryptokey = recv[4];
 
