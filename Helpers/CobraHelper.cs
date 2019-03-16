2b32e0a0aee0fff70bc9b4c2348a4ae88c529b82
Michael Baart <michael.baart96@gmail.com>
2019-03-15 17:17:45 -0700

    Add async

--- Helpers/CobraHelper.cs
+++ Helpers/CobraHelper.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -15,18 +16,19 @@ namespace Titanoboa
     {
         private static string QuoteSrvEndpoint = "https://localhost:5001/quote";
         
-        public static decimal GetQuote(User user, string stockSymbol)
+        public static async Task<decimal> GetQuote(User user, string stockSymbol)
         {
             decimal amount;
             string cryptokey;
 
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(QuoteSrvEndpoint + "/" + user.Username + "/" + stockSymbol + "");
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var request = WebRequest.Create(QuoteSrvEndpoint + "/" + user.Username + "/" + stockSymbol + "");
+            var response = await request.GetResponseAsync();
             string responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             
-            string[] recv = responseString.Split(",");
-            amount = decimal.Parse(recv[0]);
-            cryptokey = recv[1];
+            JObject responseJson = JObject.Parse(responseString);
+
+            amount = decimal.Parse(responseJson["amount"].ToString());
+            cryptokey = responseJson["cryptokey"].ToString();
 
             var logger = new Logger();
             logger.LogQuoteServer(user, amount, stockSymbol, user.Username, DateTime.Now.ToString(), cryptokey);
